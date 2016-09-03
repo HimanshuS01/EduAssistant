5af04f091cda4e2f86fb62ad61d577a98288602e
Himanshu singhal <himanshu.singhal116@gmail.com>
2016-09-03 18:08:34 +0530

    Recycler View and fab

--- app/src/main/java/com/codingblocks/customnavigationdrawer/StudentBatches.java
+++ app/src/main/java/com/codingblocks/customnavigationdrawer/StudentBatches.java
@@ -3,6 +3,7 @@ package com.codingblocks.customnavigationdrawer;
 import android.app.Activity;
 import android.app.Fragment;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AlertDialog;
@@ -10,7 +11,13 @@ import android.text.InputType;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by HIman$hu on 8/6/2016.
@@ -18,6 +25,9 @@ import android.widget.EditText;
 public class StudentBatches extends Fragment {
 
     String m_Text;
+    ListView batches_list_view;
+    List<String> dataList;
+    ArrayAdapter<String> adapter;
 
     public StudentBatches() {
         // Required empty public constructor
@@ -30,9 +40,11 @@ public class StudentBatches extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_friends, container, false);
+
+        batches_list_view = (ListView) rootView.findViewById(R.id.Batches_List_View);
+        dataList = new ArrayList<>();
 
         FloatingActionButton fab = (FloatingActionButton) rootView.findViewById(R.id.fab);
         fab.setBackgroundDrawable(getResources().getDrawable(R.drawable.plus_icon));
@@ -47,7 +59,7 @@ public class StudentBatches extends Fragment {
                 final EditText input = new EditText(getActivity());
 
                 // Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
-                input.setInputType(InputType.TYPE_CLASS_TEXT );
+                input.setInputType(InputType.TYPE_CLASS_TEXT);
                 builder.setView(input);
 
                 // Set up the buttons
@@ -55,6 +67,9 @@ public class StudentBatches extends Fragment {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         m_Text = input.getText().toString();
+                        dataList.add(m_Text);
+                        adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1, dataList);
+                        batches_list_view.setAdapter(adapter);//After this step you should be able to see the data in your list view.
                     }
                 });
                 builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
@@ -68,6 +83,32 @@ public class StudentBatches extends Fragment {
             }
         });
 
+        batches_list_view.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, final int position, long id) {
+                android.app.AlertDialog.Builder builder = new android.app.AlertDialog.Builder(getActivity());
+                builder.setTitle("Delete");
+                builder.setMessage("Are you sure you want to delete it?");
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dataList.remove(dataList.get(position));
+                        adapter.notifyDataSetChanged();
+                    }
+                });
+                builder.create().show();
+                return true;
+            }
+        });
+
+        batches_list_view.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent();
+                intent.setClass(getActivity(), StudentListRecyclerView.class);
+                startActivity(intent);
+            }
+        });
         // Inflate the layout for this fragment
         return rootView;
     }
